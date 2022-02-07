d9f4a1e9b68eb1b3ddb49f30e60bd994c26f610e
bankonly <for21bn01@gmail.com>
2022-02-07 17:15:45 +0700

    mailer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
-    socket: require("./utils/socket"),
-    Res: require("./utils/api-response"),
-    aws: require("./utils/aws"),
-    validator: require("./utils/body-validation"),
-    handler: require("./utils/handler"),
-    query: require("./utils/query"),
-    helper: require("./utils/helper"),
-    server: require("./utils/server")
-}
+  socket: require("./utils/socket"),
+  Res: require("./utils/api-response"),
+  aws: require("./utils/aws"),
+  validator: require("./utils/body-validation"),
+  handler: require("./utils/handler"),
+  query: require("./utils/query"),
+  helper: require("./utils/helper"),
+  server: require("./utils/server"),
+  mailer: require("./utils/mail"),
+};
