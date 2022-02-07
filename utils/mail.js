d858960271f901f88f839ee03c23b717624556b7
bankonly <for21bn01@gmail.com>
2022-02-07 20:38:05 +0700

    mail update

--- utils/mail.js
+++ utils/mail.js
@@ -17,44 +17,47 @@ const mailConfig = ({ email, service, password, limitMinutePerRequest = 1 }) => 
   minutePerRequest = limitMinutePerRequest;
 };
 
-const send = async ({ to, text = "Greeting!!", subject = "SUBJECT", from = null, otp_code, link, enableLog = true }) => {
+const send = async ({ to, text = "Greeting!!", subject = "SUBJECT", from = null, otp_code, link, enableLog = true, tooManySendMessage }) => {
   if (!transporter) throw new Error(`Invalid configuration mailConfig`);
-  try {
-    const mailSendOption = {
-      from: from == null ? process.env.MAIL_ID : from,
-      to: to,
-      subject: subject,
-      text: text,
-      html:
-        link ||
-        `
+  const mailSendOption = {
+    from: from == null ? process.env.MAIL_ID : from,
+    to: to,
+    subject: subject,
+    text: text,
+    html:
+      link ||
+      `
       <h3>Your verify code</h3>
       <h1>${otp_code}</h1>
       `,
+  };
+
+  if (enableLog) {
+    const now_date = new Date();
+    const logData = {
+      email: to,
+      created_at: now_date.getTime(),
     };
+    let created_at = logData.created_at;
+    const emailIndex = sendOtpLog.findIndex((e) => e.email === to);
+    if (emailIndex !== -1) {
+      created_at = sendOtpLog[emailIndex].created_at;
+      const minutes = Math.floor((logData.created_at - created_at) / 60000);
+      if (minutes >= minutePerRequest) {
+        sendOtpLog[emailIndex].created_at = logData.created_at
+      } else {
+        throw new Error(`400-MAIL429::${tooManySendMessage || `Please wait after ${minutePerRequest} minutes and try again`}`)
+      }
+    } else {
+      sendOtpLog.push(logData);
+    }
+
+    fs.writeFileSync(__dirname + "/../data-sets/send-otp.json", JSON.stringify(sendOtpLog));
+  }
+  try {
 
     const sendMail = await transporter.sendMail(mailSendOption);
     if (sendMail.error) throw new Error("Mail failed");
-
-    if (enableLog) {
-      const now_date = new Date();
-      const logData = {
-        email: to,
-        created_at: now_date.getTime(),
-      };
-      let created_at = logData.created_at;
-      const emailIndex = sendOtpLog.findIndex((e) => e.email === to);
-      if (emailIndex !== -1) {
-        created_at = sendOtpLog[emailIndex].created_at;
-        const minutes = Math.floor((now - created_at) / 60000);
-        console.log(minutes);
-      } else {
-        sendOtpLog.push(logData);
-      }
-
-      fs.writeFileSync(__dirname + "/../data-sets/send-otp.json", JSON.stringify(sendOtpLog));
-      console.log(sendOtpLog);
-    }
   } catch (error) {
     console.log(error.message);
     throw new Error(`400::MAILER500`);
